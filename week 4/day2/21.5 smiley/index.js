f80d7d2d49f4a7257c8e885c350e7b76187c461e
Talwind1 <talwind1@gmail.com>
2021-11-30 08:43:46 +0200

    smiley

--- week 4/day2/21.5 smiley/index.js
+++ week 4/day2/21.5 smiley/index.js
@@ -1,12 +1,19 @@
 let submit = document.querySelector(".submit");
-let num = document.getElementsByName("smilies").values;
+let num = document.getElementsByName("smilies").value;
+num = parseInt(num);
+
+let container = document.querySelector(".container");
 
 submit.addEventListener("click", () => {
-  if (!Number.isInteger(num)) {
+  console.log(typeof num);
+  if (Number.isInteger(num)) {
+    for (let i = 0; i < num; i++) {
+      let img = document.createElement("img");
+      img.src = "/img/smiley-6323863_960_720.png";
+      document.body.appendChild(img);
+      container.appendChild(img);
+    }
+  } else {
     alert("Enter a valid number please");
-  }
-  for (let i = 0; i < num; i++) {
-    let img = document.createElement("img");
-    img.src = "/.";
   }
 });
