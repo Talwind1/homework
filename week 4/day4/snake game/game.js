13160bf3a2cfa37fe1d665d072d39f2b2dd8f1d0
Talwind1 <talwind1@gmail.com>
2021-12-01 21:44:44 +0200

    snake

--- week 4/day4/snake game/game.js
+++ week 4/day4/snake game/game.js
@@ -2,14 +2,26 @@ import {
   update as updateSnake,
   draw as drawSnake,
   SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
 } from "./snake.js";
 
 import { update as updateFood, draw as drawFood } from "./food.js";
 
+import { outsideGrid } from "./grid.js";
+
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
+  if (gameOver) {
+    if (confirm("You lost. Press ok to restart.")) {
+      window.location = "/";
+    }
+    return;
+  }
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -23,8 +35,12 @@ window.requestAnimationFrame(main);
 function update() {
   updateSnake();
   updateFood();
+  checkDeath();
 }
 
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
 function draw() {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
--- week 4/day4/snake game/snake.js
+++ week 4/day4/snake game/snake.js
@@ -29,12 +29,20 @@ export function expandSnake(amount) {
   newSegments += amount;
 }
 
-export function onSnake(position) {
-  return snakeBody.some((segment) => {
+export function onSnake(position, ignoredHead = false) {
+  return snakeBody.some((segment, index) => {
+    if (ignoredHead && index === 0) return false;
     return equalPositions(segment, position);
   });
 }
 
+export function getSnakeHead() {
+  return snakeBody[0];
+}
+
+export function snakeIntersection() {
+  return onSnake(snakeBody[0], true);
+}
 function equalPositions(pos1, pos2) {
   return pos1.x === pos2.x && pos1.y === pos2.y;
 }
