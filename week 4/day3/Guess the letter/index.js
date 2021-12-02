a76494d11ed3ccafce6c21b0b92bbb8df4490e36
Talwind1 <talwind1@gmail.com>
2021-12-02 07:09:20 +0200

    start

---
+++ week 4/day3/Guess the letter/index.js
@@ -0,0 +1,29 @@
+const range = 'z'-'a';
+let valChar = Math.floor(Math.random() * range);
+valChar = a + valChar;
+const char = //convert asci code to char
+//create a random letter
+
+const guess = window.addEventListener(keypress, function (e) {
+    const guess = e.key;
+
+
+    if (/*not a char*/) {
+      this.alert("nope, ")
+    } 
+    else { //if it is a char 
+
+      if ((guess !== letter)) {
+        if (Array wrong include guess){
+
+        }
+        else{add guess to Array}
+
+      }
+    }
+  });
+
+
+// function checkChar(event) {
+//   e.targe;
+// }
