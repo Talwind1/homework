2d873b0c1d905d5615a9d27dfcf7fa7e0cb03cc5
Talwind1 <talwind1@gmail.com>
2021-12-06 11:47:06 +0200

    25.1

---
+++ week 4/day3/22.1 form/script.js
@@ -0,0 +1,7 @@
+// function onButtonClick(){
+document.getElementById("textInput").className = "show";
+// }
+
+document.getElementById("submit").addEventListener((e) => {
+  document.getElementById("container").className = "hide";
+});
--- week 4/day3/Guess the letter/index.js
+++ week 4/day3/Guess the letter/index.js
@@ -1,29 +1,70 @@
-const range = 'z'-'a';
-let valChar = Math.floor(Math.random() * range);
-valChar = a + valChar;
-const char = //convert asci code to char
-//create a random letter
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-const guess = window.addEventListener(keypress, function (e) {
-    const guess = e.key;
+  console.log(getRandomLetter());
+  function getRandomLetter(){
+    let valChar = Math.floor(Math.random() * letters.length);
+    let winnerLetter = letters[valChar];
+      return winnerLetter;
+  }
 
-
-    if (/*not a char*/) {
-      this.alert("nope, ")
-    } 
-    else { //if it is a char 
-
-      if ((guess !== letter)) {
-        if (Array wrong include guess){
-
-        }
-        else{add guess to Array}
-
+  console.log(getRandomLetter());
+  
+  
+  let message = document.getElementsByClassName('message');
+  
+  
+  let guess = window.addEventListener(keypress, function (e) {
+      if(!e.key.isChar()){
+        notChar();
       }
-    }
-  });
-
-
-// function checkChar(event) {
-//   e.targe;
-// }
+  
+  
+    //   if (/*not a char*/) {
+    //     this.alert("nope, ")
+    //   } 
+    //   else { //if it is a char 
+  
+    //     if ((guess !== letter)) {
+    //       if (Array wrong include guess){
+  
+    //       }
+    //       else{add guess to Array}
+  
+    //     }
+    //   }
+    // });
+  
+  // function isChar(char){
+  //   return (char >= 'a' && char <='z')||(char >= 'A' && char <='Z')
+  // }
+  
+  // function notChar(){
+  //   message.innerText ="please enter a valid letter"
+  // }
