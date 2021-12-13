1715c557597434ced8d60b7f3c3bd740d3eba48b
Talwind1 <talwind1@gmail.com>
2021-12-13 15:35:29 +0200

    goats farms

--- week 6/jsx/src/App.js
+++ week 6/jsx/src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
-import Button from "./components/Button";
+import Card from "./components/Card";
+import pic1 from "./pics/1.jpg";
+import pic2 from "./pics/2.jpg";
+import pic3 from "./pics/3.jpg";
 
 function App() {
   return (
     <div className="App">
-      <Button content="Important" />
-
-      <Button content="Not Important" />
+      <Card details=" Yodfat" link="http://goatswiththewind.com/" pic={pic1} />
+      <Card
+        details="Kishorit"
+        link="https://kishorit.org.il/en/anafim/goat-dairy"
+        pic={pic2}
+      />
+      <Card
+        details=" Noat"
+        link="https://www.naotfarm.co.il/en/about-us/"
+        pic={pic3}
+      />
     </div>
   );
 }
--- week 6/jsx/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-//import react from "react";
-
-const Button = (props) => {
-  let className = props.content.toLowerCase().split(" ")[0];
-
-  return <button className={className}>{props.content}</button>;
-};
-
-export default Button;
