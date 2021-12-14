2649fc6547e8ce7703ccdc983f4c26b9a9b7fde3
Talwind1 <talwind1@gmail.com>
2021-12-14 11:10:57 +0200

    counter state

--- week 6/jsx/src/App.js
+++ week 6/jsx/src/App.js
@@ -1,12 +1,30 @@
+import React from "react";
+import { render } from "react-dom";
 import "./App.css";
-import Quiz from "./components/Quiz";
 
-function App() {
-  return (
-    <div className="App">
-      <Quiz />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+    //this.increase = increase.bind(this);
+  }
+
+  increase = () => {
+    this.setState((prevState) => {
+      return { counter: prevState.counter + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.increase}> increament</button>
+        <h2>{this.state.counter}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
--- week 6/jsx/src/components/Q1Title.js
+++
@@ -1,7 +0,0 @@
-import react from "react";
-
-const Q1Title = () => {
-  return <div className="q1-title">How much you love front end?</div>;
-};
-
-export default Q1Title;
