be3f6865832a750885e01ad5f67fc4f27db71a48
Talwind1 <talwind1@gmail.com>
2021-12-14 20:55:51 +0200

    box

--- week 6/jsx/src/App.js
+++ week 6/jsx/src/App.js
@@ -1,40 +1,58 @@
-import './App.css';
-import React from 'react'
+import "./App.css";
+import React from "react";
 
-import './App.css'
-
+import "./App.css";
 
 class App extends React.Component {
-    
-  constructor(props){
-    super(props)
-    this.state ={status: ''}
+  constructor() {
+    super();
+    this.state = {
+      color: null,
+      border: null,
+      counter: 0,
+    };
+    this.colors = ["blue", "red", "green"];
   }
-  
-  componentDidMount(){
-    setTimeout(()=>{
-      this.setState({status:'showBox'})
+  chooseColor = () => {
+    let i = Math.floor(Math.random() * this.colors.length);
+    let color = this.colors[i];
+    return color;
+  };
 
-    },2000)
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        color: this.chooseColor(),
+        counter: this.state.counter + 1,
+      });
+    }, 500);
   }
-  componentDidUpdate(){
-    setTimeout(()=>{
-      this.setState({status:''})
 
-    },4000)
+  componentDidUpdate() {
+    if (this.state.counter % 4 === 0) {
+      this.setState({
+        color: this.chooseColor(),
+        counter: this.state.counter + 1,
+        border: "makeCircle",
+      });
+    } else {
+      setTimeout(() => {
+        this.setState({
+          color: this.chooseColor(),
+          counter: this.state.counter + 1,
+        });
+      }, 500);
+    }
   }
-  
-  render (){
-    return(
-      <div className="boxContainer">
-        <div className={this.state.status} style={{width:'300px' ,height:'300px'}}></div>
-        <div className={this.state.status} style={{width:'200px' ,height:'200px'}}></div>
-        <div className={this.state.status} style={{width:'150px' ,height:'150px'}}></div>
 
-  </div>
-  )
-  }      
-  
+  render() {
+    return (
+      <div
+        id="box"
+        className={`${this.state.color} ${this.state.border}`}
+      ></div>
+    );
+  }
 }
 
 export default App;
