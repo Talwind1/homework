6994133d52d3f5c3192404d4a56ec6755e61b00e
Talwind1 <talwind1@gmail.com>
2021-12-19 23:37:46 +0200

    data massaging

---
+++ week 7/my_app/src/Card.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Card = (props) => {
+  return <div> {props.older} </div>;
+};
+
+export default Card;
---
+++ week 7/my_app/src/Name.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Name = (props) => {
+  return <div> {props.name} </div>;
+};
+
+export default Name;
