2dbcb3cee1f6899cff02be35cc51b5223fc4566e
Talwind1 <talwind1@gmail.com>
2021-12-20 00:15:07 +0200

    avatars

--- week 7/my_app/src/Card.js
+++
@@ -1,7 +0,0 @@
-import react from "react";
-
-const Card = (props) => {
-  return <div> {props.older} </div>;
-};
-
-export default Card;
--- week 7/my_app/src/Name.js
+++
@@ -1,7 +0,0 @@
-import react from "react";
-
-const Name = (props) => {
-  return <div> {props.name} </div>;
-};
-
-export default Name;
