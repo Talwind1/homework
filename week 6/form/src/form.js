3fa36268d7898d593bebd543bddaffb6ec9e010c
Talwind1 <talwind1@gmail.com>
2021-12-19 15:08:10 +0200

    chuck noris 1

---
+++ week 6/form/src/form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <label>Name</label>
+        <input type="text" />
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+export default Form;
