843ff6a5df0ecf7cc8aecf5cce5aceda82b4b78a
Talwind1 <talwind1@gmail.com>
2021-12-26 17:14:30 +0200

    18.2

--- week 8/exercise/src/App.js
+++ week 8/exercise/src/App.js
@@ -1,15 +1,49 @@
 import "./App.css";
-import Text from "./Text";
+import react, { useState } from "react";
 
 function App() {
-  let text = "tushtushtushtushtush ata rach";
-  let size = 10;
+  const dataObj = [
+    { name: "CSS", completed: true },
+    { name: "JavaScript", completed: true },
+    { name: "Learn React", completed: false },
+    { name: "Learn mongoDB", completed: false },
+    { name: "Learn Node JS", completed: false },
+  ];
+  const [data, setData] = useState(dataObj);
 
-  return (
-    <div className="App">
-      <Text text={text} size={size} />
-    </div>
-  );
+  const handleStatus = (index) => {
+    const newData = [...data];
+    newData[index].completed = !data[index].completed;
+    setData(newData);
+  };
+
+  const checkStatus = (isCompleted) => {
+    let icon = isCompleted ? "✅" : "🟩";
+    let style = isCompleted
+      ? { textDecoration: "line-through" }
+      : { textDecoration: "none" };
+    return { icon, style };
+  };
+
+  const renderList = () => {
+    return data.map((item, index) => {
+      return (
+        <div key="title">
+          <span
+            onClick={() => {
+              handleStatus(index);
+            }}
+          >
+            {checkStatus(item.completed).icon}
+          </span>
+
+          <h2 style={checkStatus(item.completed).style}>{item.name}</h2>
+        </div>
+      );
+    });
+  };
+
+  return <div className="App">{renderList()}</div>;
 }
 
 export default App;
