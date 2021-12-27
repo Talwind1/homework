95e72159d09e42278876c16dc3b86411111617e7
Talwind1 <talwind1@gmail.com>
2021-12-27 16:50:09 +0200

    19.2 fix

--- week 8/exercise/src/App.js
+++ week 8/exercise/src/App.js
@@ -3,72 +3,43 @@ import axios from "axios";
 import react, { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [categories, setCategories] = useState(null);
-  const [categoryJoke, setCategoryJoke] = useState("");
-  const [loading, setLoading] = useState(false);
-
+  const [countries, setCountries] = useState(null);
+  const [term, setTerm] = useState("");
   useEffect(() => {
-    const generateCategories = async () => {
-      setLoading(true);
-      try {
-        const { data } = await axios.get(
-          `https://api.chucknorris.io/jokes/categories`
-        );
-        setCategories(data);
-        console.log(categories);
-        setLoading(false);
-      } catch (e) {
-        throw e.messege;
-      }
+    const searchCountry = async () => {
+      const { data } = await axios.get("https://restcountries.com/v2/all");
+      setCountries(data);
+      console.log(countries);
     };
-    generateCategories();
+    searchCountry();
   }, []);
 
-  const searchCategory = async (category) => {
-    try {
-      setLoading(true);
-      const { data } = await axios.get(
-        `https://api.chucknorris.io/jokes/random?category=${category}`
-      );
-      setCategoryJoke(data.value);
-      console.log(categoryJoke);
-      setLoading(false);
-    } catch (e) {
-      throw e.messege;
-    }
-  };
-  const randomJoke = async () => {
-    setLoading(true);
-    try {
-      const { data } = await axios.get(
-        `https://api.chucknorris.io/jokes/random`
-      );
-      setData(data);
-
-      setLoading(false);
-    } catch (e) {
-      throw e.messege;
-    }
+  const showCountries = () => {
+    return countries.map((element) => {
+      return <li key={element.name}>{element.name}</li>;
+    });
   };
 
+  const updateTerm = (value) => {
+    setTerm(value);
+
+    const filterCountries = countries.filter((item) =>
+      item.name.toLowerCase().startsWith(term.toLocaleLowerCase())
+    );
+    setCountries(filterCountries);
+  };
+  const filterDisplay = () => {};
   return (
     <div className="App">
-      <button onClick={randomJoke}>Make me laugh</button>
-      {loading && <h2>Loading...</h2>}
-      {data && <h3>{data.value}</h3>}
-      {categories && (
-        <div>
-          {categories.map((category) => {
-            return (
-              <button onClick={() => searchCategory(category)}>
-                {category}
-              </button>
-            );
-          })}
-          <h2>{categoryJoke}</h2>
-        </div>
+      {countries && (
+        <input
+          type="text"
+          onChange={(e) => updateTerm(e.target.value)}
+          value={term}
+        />
       )}
+      {/* <button onClick={filterDisplay}>Click</button> */}
+      {countries && <div> {showCountries()} </div>}
     </div>
   );
 }
