4df26b297667c7359a491f0998625af3854759d0
Talwind1 <talwind1@gmail.com>
2021-12-28 17:43:29 +0200

    21.3

--- week 8/exercise/src/App.js
+++ week 8/exercise/src/App.js
@@ -1,22 +1,41 @@
 import "./App.css";
 import react, { useState, useEffect, useRef } from "react";
-import video from "./earth.mp4";
+import santa from "./santa.png";
+import shrimp from "./shrimp.jpg";
 
 function App() {
-  const videoRef = useRef();
+  const santaRef = useRef();
 
-  const handlePlay = () => {
-    videoRef.current.play();
-  };
-  const handlePause = () => {
-    videoRef.current.pause();
-  };
+  const shrimpRef = useRef();
 
+  const [santaStyle, setSantaStyle] = useState({ filter: "grayscale(100%)" });
+  const [shrimpStyle, setShrimpStyle] = useState({ filter: "grayscale(100%)" });
+
+  const handleOver = (refItem) => {
+    if (refItem === "santaRef") {
+      setSantaStyle;
+    }
+    refItem.current.style.filter = "grayscale(100%)";
+  };
+  const handleLeave = (refItem) => {
+    refItem.current.style.filter = "grayscale(0%)";
+  };
   return (
     <div className="App">
-      <video src={video} ref={videoRef} />
-      <button onClick={handlePlay}>play</button>
-      <button onClick={handlePause}>pause</button>
+      <img
+        src={santa}
+        alt="santa"
+        ref={santaRef}
+        onMouseOver={() => handleOver(santaRef)}
+        onMouseLeave={() => handleLeave(santaRef)}
+      />
+      <img
+        src={shrimp}
+        alt="shrimp"
+        ref={shrimpRef}
+        onMouseOver={() => handleOver(shrimpRef)}
+        onMouseLeave={() => handleLeave(shrimpRef)}
+      />
     </div>
   );
 }
