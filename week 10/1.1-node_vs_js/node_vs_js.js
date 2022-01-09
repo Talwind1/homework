cb1c5b5067dbee85f9393915907b669825a9f443
Talwind1 <talwind1@gmail.com>
2022-01-09 12:40:49 +0200

    3 differances between js and node js

---
+++ week 10/1.1-node_vs_js/node_vs_js.js
@@ -0,0 +1,25 @@
+// 3 things node.js has that the client side javascript doesn’t have: (in the browser)
+
+// 1. Browser has window object
+// node.js has the global object.
+//because windows is part of the browser environment.
+//node.js doesn't run on the browser.
+
+//2. process object: it exist just in node.js.
+// browser has document (like documeny query selector)
+//
+
+//3. node.js has file system , js doesn't has it.
+
+//4.(Extra): node.js doesn't have an access to local storage. Js has an acess to local storage.
+
+//3 diffrences between js and node.js: (in general)
+
+//1. NodeJS is a Javascript runtime environment,
+//Javascript is a programming language that is used for writing scripts on the website.
+
+//2. Javascript is a programming language that is used for writing scripts on the website.
+//Javascript can run in any browser engine as like JS core in safari and Spidermonkey in Firefox.
+
+//3.Nodejs does not have capability to add HTML tags.	It is mostly used on the server-side.
+//	Javascript is capable enough to add HTML and play with the DOM. 	It is basically used on the client-side.
