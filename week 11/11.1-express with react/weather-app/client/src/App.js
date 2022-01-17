61403c819053fadb646995a9963c41c2e9a9e5ca
Talwind1 <talwind1@gmail.com>
2022-01-17 17:00:40 +0200

    15.1 create mongodb data base

--- week 11/11.1-express with react/weather-app/client/src/App.js
+++ week 11/11.1-express with react/weather-app/client/src/App.js
@@ -1,19 +1,29 @@
 // import logo from "./logo.svg";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
   useEffect(() => {
     fetch = async () => {
-      const { data } = await axios.get(
-        "http://localhost:3001/weather?address=boston"
-      );
-      console.log(data);
+      try {
+        const { data } = await axios.get(
+          "http://localhost:3001/weather?address=boston"
+        );
+        console.log(data);
+        setData(data);
+      } catch (e) {
+        throw Error(e.message);
+      }
     };
     fetch();
   }, []);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>{data.forecast}</h1>
+    </div>
+  );
 }
 
 export default App;
--- week 11/11.1-express with react/weather-app/server/app.js
+++ week 11/11.1-express with react/weather-app/server/app.js
@@ -1,60 +1,62 @@
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./src/utils/geocode')
-const forecast = require('./src/utils/forecast')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./src/utils/geocode");
+const forecast = require("./src/utils/forecast");
+const cors = require("cors");
+const app = express();
 app.use(cors());
 // Setup static directory to serve
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
-            return res.send({ error })
+          return res.send({ error });
         }
 
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
     }
+  );
+});
 
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
 
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
 
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Andrew Mead',
-        errorMessage: 'Page not found.'
-    })
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Andrew Mead",
+    errorMessage: "Page not found.",
+  });
+});
 
 app.listen(3001, () => {
-    console.log('Server is up on port 3001.')
-})
+  console.log("Server is up on port 3001.");
+});
